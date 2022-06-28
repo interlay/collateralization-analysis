8970775f97c74653796eb7c91fa370d3934791e7
Philipp Bunke <philippbunke@gmail.com>
2022-06-28 22:19:41 +0200

    Adding the code lol

---
+++ data_request.py
@@ -0,0 +1,121 @@
+import requests
+from datetime import datetime
+import pandas as pd
+
+urls = {
+    "coingecko": "https://api.coingecko.com/api/v3/coins/",
+}
+
+class Token():
+    def __init__(self, name:str, ticker: str):
+        self._ticker = ticker
+        self._name = name
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def ticker(self) -> str:
+        return self._ticker
+
+    @property
+    def prices(self) -> pd.DataFrame:
+        return self._prices
+
+    @prices.setter
+    def prices(self, prices: pd.DataFrame):
+        self._prices = prices
+
+
+class Token_Pair():
+    def __init__(self, base_token: Token, quote_token: Token) -> None:
+        self._base_token = base_token
+        self._quote_token = quote_token
+
+    # Getter & Setter
+
+    @property
+    def base_token(self) -> Token:
+        return self._base_token
+
+    @property
+    def quote_token(self) -> Token:
+        return self._quote_token
+
+    @property
+    def prices(self) -> pd.DataFrame:
+        return self._prices
+
+    @prices.setter
+    def prices(self, prices: pd.DataFrame) -> None:
+        self._prices = prices
+
+    @property
+    def prices(self) -> pd.DataFrame:
+        return self._prices
+
+    @property
+    def returns(self) -> pd.DataFrame:
+        return self._returns
+
+
+    # Functions
+
+    def get_prices(self, data_source: str = "coingecko", start_date: str = None, end_date: str = None) -> None:
+        request = Data_Request(self, data_source, start_date, end_date)
+        prices = request.request_historic_prices()
+        # fails here for some reason?!
+        self.prices = prices
+
+    def calculate_returns(self, type: str = "geometric", period: str = "daily") -> None:
+        shift_periods = {
+            "daily": 1,
+            "weekly" : 7,
+            "monthly": 31,
+            "yearly": 365
+        }
+        shift_period = shift_periods[period]
+
+        if type == "geometric":
+            self.returns = self.prices.pct_change(periods=shift_period, fill_method="bfill")
+
+
+class Data_Request():
+    def __init__(self, token_pair: Token_Pair, data_source: str ="coingecko", start_date: str = None, end_date: str = None):
+        """
+        :Token_Pair: An instance of class Token_Pair with the two tokens for which the data should be requested
+        :data_source: Source from where the data should be requested
+        :start_date: Start date as string in the format 'YYYY-MM-DD'
+        :end_date: End date as string in the format 'YYYY-MM-DD', will default to today is not provided
+        """
+
+        self._token_pair = token_pair
+        self._data_source = data_source
+        self._start_date = start_date
+        self._end_date = end_date if end_date is not None else datetime.today().strftime("%Y-%m-%d")
+
+    def get_length_in_days(self) -> str:
+        if self._start_date is None:
+            return str(365)
+        
+        time_delta = datetime.strptime(self._end_date, "%Y-%m-%d") - datetime.strptime(self._start_date, "%Y-%m-%d")
+        return str(time_delta.days)
+
+    def parse_url(self):
+        "Currently only supports price requests from coingecko"
+
+        length_in_days = self.get_length_in_days()
+
+        if self._data_source == "coingecko":
+            base_token = self._token_pair.base_token.name
+            quote_token = self._token_pair.quote_token.name
+            self._url_endpoint = urls[self._data_source] + base_token + "/market_chart?vs_currency=" + quote_token + "&days=" + length_in_days
+
+    def request_historic_prices(self):
+        self.parse_url()
+        price_data = requests.get(self._url_endpoint).json()["prices"]
+        price_data = pd.DataFrame(price_data, columns=["Date", "Price"])
+        price_data.set_index("Date", inplace=True)
+        price_data.index = pd.to_datetime(price_data.index, unit="ms")
+        return price_data
---
+++ main.py
@@ -0,0 +1,21 @@
+# 1. Initialize a token pair
+#     1.1 Get price data for the token pair
+#       - Query data
+#       - Parse data into data frame
+#
+# 2. Initialize a strategy
+# 3. Initialize a simulation
+# 3.1. Add the token pair to the simulation
+# 3.2 Add the strategy to the simulation
+# 3.3 Run the simulation
+
+from data_request import Token, Token_Pair, Data_Request
+
+
+quote_currency = Token("USD", "USD")
+base_currncy = Token("kintsugi","KINT")
+
+pair = Token_Pair(base_currncy, quote_currency)
+pair.get_prices(start_date="2022-01-01")
+
+
