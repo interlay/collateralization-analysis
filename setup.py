1a8e2ec47c32a313459423d1ba2b9d6f225281b9
Philipp Bunke <philippbunke@gmail.com>
2023-06-09 16:05:29 +0200

    chore: renamed constant and removed unused imports

--- setup.py
+++ setup.py
@@ -3,32 +3,32 @@ from datetime import datetime
 from substrateinterface import Keypair, SubstrateInterface
 from substrateinterface.exceptions import SubstrateRequestException
 
+from extract.tokens import extract_onchain_tokens, extract_foreign_tokens
 from chopsticks.dex import ChainDEX
 from chopsticks.state_queries import ChainGeneral
 from data.data_request import (
     Bitcoin_Dollar,
     BTC,
-    Polkadot_Dollar,
-    Thresholds,
     KSM,
     USD,
     KINT,
-    Token,
     Token_Pair,
     ToBaseDecimals,
 )
 
+#%%
+kintsugi = ChainGeneral()
+
+#%%
 kint, ksm, usd, btc = KINT(), KSM(), USD(), BTC()
 
-FUNDING = {
+ACCOUNT_FUNDING = {
     ksm: 100_000 * 10 ** ToBaseDecimals.KUSAMA.get_decimals(),
     kint: 100_000 * 10 ** ToBaseDecimals.KINTSUGI.get_decimals(),
 }
 
-kintsugi = Kintsugi()
-kintsugi.fund_account(FUNDING)
 
-
+kintsugi.fund_account(ACCOUNT_FUNDING)
 # %%
 
 kintsugi.get_thresholds()
@@ -52,6 +52,6 @@ kintsugi.update_collateral_ratios(ksm, current_ksm_price, current_btc_price)
 
 # %%
 
-dex = DEX()
+dex = ChainDEX()
 dex.exact_input_swap(100 * 10**12, 1)
 # %%
